ebae96613b94f3967d74f1c6c674c98cea6f09b7
keerthivasan123 <keerthivasan287@gmail.com>
2022-05-19 17:06:37 +0530

    Code Review changes

--- src/main/java/com/Testing/Employee.java
+++ src/main/java/com/Testing/Employee.java
@@ -1,5 +1,16 @@
 package com.Testing;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.UUID;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+
 public class Employee {
     private String id,name,password,email,country;
     public String getId() {
@@ -33,4 +44,178 @@ public class Employee {
         this.country = country;
     }
 
+    private static final String dburl = "jdbc:postgresql://localhost:5432/mydb";
+    private static final String dbuser = "postgres";
+    private static final String dbpassword = "postgres";
+
+    private static final String INSERT_USERS_SQL = "INSERT INTO employee" +
+            "  (id, name, email, country, password) VALUES " +
+            " (?, ?, ?, ?, ?);";
+
+    private static final String QUERY = "select id,name,email,country,password from employee where id =?";
+
+    private static final String SELECT_ALL_QUERY = "select * from employee";
+
+    private static final String UPDATE_USERS_SQL = "update employee set name = ?, email = ?, country = ?, password = ? where id = ?;";
+
+    private static final String DELETE_USERS_SQL = "delete from employee where id = ?;";
+
+    private static Gson gson = new Gson();
+
+    Employee(){
+    }
+
+    Employee( String id, String name, String password, String email, String country ){
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.country = country;
+    }
+
+    Employee( String id ){
+        this.id = id;
+    }
+
+    public static PreparedStatement getPreparedStatement( String query ) throws SQLException {
+            Connection connection = DriverManager.getConnection(dburl, dbuser, dbpassword);
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            return preparedStatement;
+    }
+
+    public static String getEmployeeById( String id ){
+        try {
+            PreparedStatement preparedStatement = Employee.getPreparedStatement( QUERY );
+            preparedStatement.setString(1, id);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                String password = rs.getString("password");
+                System.out.println(id + "," + name + "," + email + "," + country + "," + password);
+
+                Employee emp = new Employee( id, name, password, email, country );
+
+                String result = Employee.gson.toJson(emp);
+                return result;
+            }
+
+            JsonObject result = new JsonObject();
+            result.addProperty("error", "Employee with id: " + id + " Does't exists");
+            return result.toString();
+
+        } catch ( SQLException e ){
+            return printSQLException(e);
+        }
+    }
+
+    public static String getAllEmployee() {
+        try {
+            PreparedStatement preparedStatement = Employee.getPreparedStatement( SELECT_ALL_QUERY );
+            ResultSet rs = preparedStatement.executeQuery();
+
+            ArrayList<Employee> listOfEmployee = new ArrayList<>();
+
+            while (rs.next()) {
+                String uuid = rs.getString("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                String password = rs.getString("password");
+                System.out.println(uuid + "," + name + "," + email + "," + country + "," + password);
+
+                Employee emp = new Employee( uuid, name, password, email, country );
+
+                listOfEmployee.add(emp);
+            }
+
+            String result = Employee.gson.toJson(listOfEmployee);
+            return result;
+        } catch ( SQLException e ){
+            return printSQLException(e);
+        }
+    }
+
+    public String createEmployee(){
+        try {
+            PreparedStatement preparedStatement = Employee.getPreparedStatement( INSERT_USERS_SQL );
+            preparedStatement.setString(1, this.id);
+            preparedStatement.setString(2, this.name);
+            preparedStatement.setString(3, this.email);
+            preparedStatement.setString(4, this.country);
+            preparedStatement.setString(5, this.password);
+
+            preparedStatement.executeUpdate();
+
+            JsonObject result = new JsonObject();
+            result.addProperty("message", "Employee Successfully Created with id: " + this.id);
+            return result.toString();
+        } catch ( SQLException e ){
+            return printSQLException(e);
+        }
+    }
+
+    public String updateEmployeeById(){
+        try {
+            PreparedStatement preparedStatement = Employee.getPreparedStatement( UPDATE_USERS_SQL );
+            preparedStatement.setString(1, this.name);
+            preparedStatement.setString(2, this.email);
+            preparedStatement.setString(3, this.country);
+            preparedStatement.setString(4, this.password);
+            preparedStatement.setString(5, this.id);
+
+            preparedStatement.executeUpdate();
+
+            JsonObject result = new JsonObject();
+            result.addProperty("message", "Employee Updated Successfully.");
+            return result.toString();
+
+        } catch ( SQLException e ){
+            return printSQLException(e);
+        }
+    }
+
+    public static String deleteEmployeeById( String id ){
+        try {
+            PreparedStatement preparedStatement = Employee.getPreparedStatement( DELETE_USERS_SQL );
+            preparedStatement.setString(1, id);
+
+            int affected = preparedStatement.executeUpdate();
+            System.out.println("Number of records affected :: " + affected);
+            JsonObject result = new JsonObject();
+
+            if( affected == 0 ) {
+                result.addProperty( "error", "Invalid Employee Id");
+            } else {
+                result.addProperty( "message", "Employee with id: " + id + " Successfully Deleted");
+            }
+            return result.toString();
+        } catch ( SQLException e ){
+            return printSQLException(e);
+        }
+    }
+
+    public static String printSQLException( SQLException ex ) {
+        JsonObject result = new JsonObject();
+        for (Throwable e: ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                result.addProperty("SQLState", ((SQLException) e).getSQLState().toString());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                result.addProperty("ErrorCode", ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                result.addProperty("message", e.getMessage().toString());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    result.addProperty("Cause", t.getMessage().toString());
+                    t = t.getCause();
+                }
+            }
+        }
+        return result.toString();
+    }
 }
--- src/main/java/com/Testing/EmployeeServlet.java
+++ src/main/java/com/Testing/EmployeeServlet.java
@@ -7,114 +7,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.UUID;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 
 public class EmployeeServlet extends HttpServlet  {
-    private final String url = "jdbc:postgresql://localhost:5432/mydb";
-    private final String user = "postgres";
-    private final String password = "postgres";
-
-    private static final String INSERT_USERS_SQL = "INSERT INTO employee" +
-            "  (id, name, email, country, password) VALUES " +
-            " (?, ?, ?, ?, ?);";
-
-    private static final String QUERY = "select id,name,email,country,password from employee where id =?";
-
-    private static final String SELECT_ALL_QUERY = "select * from employee";
-
-    private static final String UPDATE_USERS_SQL = "update employee set name = ?, email = ?, country = ?, password = ? where id = ?;";
-
-    private static final String DELETE_USERS_SQL = "delete from employee where id = ?;";
-
-    private Gson gson = new Gson();
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("--post--");
+        System.out.println("--get--");
+
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter printWriter = resp.getWriter();
+
         String id = req.getParameter("id");
+
         if( id != null ) {
-            try (Connection connection = DriverManager.getConnection(url, user, password);
-                 PreparedStatement preparedStatement = connection.prepareStatement(QUERY);) {
-                preparedStatement.setString(1, id);
-                System.out.println(preparedStatement);
-
-                ResultSet rs = preparedStatement.executeQuery();
-                PrintWriter printWriter=resp.getWriter();
-                resp.setContentType("application/json");
-                resp.setCharacterEncoding("UTF-8");
-
-                int flag = 1;
-                // Step 4: Process the ResultSet object.
-                while (rs.next()) {
-                    flag = 0;
-                    String name = rs.getString("name");
-                    String email = rs.getString("email");
-                    String country = rs.getString("country");
-                    String password = rs.getString("password");
-                    System.out.println(id + "," + name + "," + email + "," + country + "," + password);
-
-                    Employee emp = new Employee();
-                    emp.setId(id);
-                    emp.setName(name);
-                    emp.setCountry(country);
-                    emp.setPassword(password);
-                    emp.setEmail(email);
-
-                    String result = this.gson.toJson(emp);
-                    printWriter.print(result);
-                }
-
-                if( flag == 1 ) {
-                    JsonObject result = new JsonObject();
-                    result.addProperty("error", "Employee with id: " + id + " Does't exists");
-
-                    printWriter.print(result.toString());
-                }
-                printWriter.flush();
-            } catch (SQLException e) {
-                printSQLException(e, resp);
-            }
+            String result = Employee.getEmployeeById( id );
+            printWriter.print(result);
         } else {
-            try (Connection connection = DriverManager.getConnection(url, user, password);
-                 PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_QUERY);) {
-                System.out.println(preparedStatement);
-
-                ResultSet rs = preparedStatement.executeQuery();
-                PrintWriter printWriter=resp.getWriter();
-                resp.setContentType("application/json");
-                resp.setCharacterEncoding("UTF-8");
-
-                ArrayList<Employee> listOfEmployee = new ArrayList<>();
-
-                while (rs.next()) {
-                    String uuid = rs.getString("id");
-                    String name = rs.getString("name");
-                    String email = rs.getString("email");
-                    String country = rs.getString("country");
-                    String password = rs.getString("password");
-                    System.out.println(uuid + "," + name + "," + email + "," + country + "," + password);
-
-                    Employee emp = new Employee();
-                    emp.setId(uuid);
-                    emp.setName(name);
-                    emp.setCountry(country);
-                    emp.setPassword(password);
-                    emp.setEmail(email);
-
-                    listOfEmployee.add(emp);
-                }
-
-                String result = this.gson.toJson(listOfEmployee);
-                printWriter.print(result);
-                printWriter.flush();
-
-            } catch (SQLException e) {
-                printSQLException(e, resp);
-            }
+            String result = Employee.getAllEmployee();
+            printWriter.print(result);
         }
+        printWriter.flush();
     }
 
     @Override
@@ -123,47 +36,24 @@ public class EmployeeServlet extends HttpServlet  {
         UUID uuid=UUID.randomUUID();
         String id = uuid.toString();
         String name = req.getParameter("name");
-        String psw = req.getParameter("password");
+        String password = req.getParameter("password");
         String email = req.getParameter("email");
         String country = req.getParameter("country");
 
-        System.out.println("Body: " + id + " " + name + " " + psw + " " + email + " " + country );
+        System.out.println("Body: " + id + " " + name + " " + password + " " + email + " " + country );
 
-        System.out.println(INSERT_USERS_SQL);
-        try (Connection connection = DriverManager.getConnection(url, user, password);
-             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
-            preparedStatement.setString(1, id);
-            preparedStatement.setString(2, name);
-            preparedStatement.setString(3, email);
-            preparedStatement.setString(4, country);
-            preparedStatement.setString(5, psw);
-
-            System.out.println(preparedStatement);
-            preparedStatement.executeUpdate();
-
-            PrintWriter printWriter=resp.getWriter();
-            resp.setContentType("application/json");
-            resp.setCharacterEncoding("UTF-8");
-
-            JsonObject result = new JsonObject();
-            result.addProperty("message", "Employee Successfully Created with id: " + id);
-
-            printWriter.print(result.toString());
-            printWriter.flush();
-        } catch (SQLException e) {
-
-            // print SQL exception information
-            printSQLException(e, resp);
-        }
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter printWriter = resp.getWriter();
+        Employee employee = new Employee( id, name, password, email, country);
+        String result = employee.createEmployee();
+        printWriter.print(result);
+        printWriter.flush();
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println(UPDATE_USERS_SQL);
-        PrintWriter printWriter=resp.getWriter();
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding("UTF-8");
-        JsonObject result = new JsonObject();
+        System.out.println("--put--");
 
         String id = req.getParameter("id");
         String name = req.getParameter("name");
@@ -172,90 +62,27 @@ public class EmployeeServlet extends HttpServlet  {
         String country = req.getParameter("country");
 
         System.out.println("Body: " + id + " " + name + " " + psw + " " + email + " " + country );
-        if( id != null ) {
-            try (Connection connection = DriverManager.getConnection(url, user, password);
 
-                 PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_USERS_SQL)) {
-                preparedStatement.setString(1, name);
-                preparedStatement.setString(2, email);
-                preparedStatement.setString(3, country);
-                preparedStatement.setString(4, psw);
-                preparedStatement.setString(5, id);
-
-                preparedStatement.executeUpdate();
-
-                result.addProperty("message", "Employee Updated Successfully.");
-            } catch (SQLException e) {
-
-                // print SQL exception information
-                printSQLException(e, resp);
-            }
-        } else {
-            result.addProperty("error", "Please Provide Employee Id");
-        }
-
-        printWriter.print(result.toString());
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter printWriter = resp.getWriter();
+        Employee employee = new Employee( id, name, psw, email, country);
+        String result = employee.updateEmployeeById();
+        printWriter.print(result);
         printWriter.flush();
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println(DELETE_USERS_SQL);
-        PrintWriter printWriter=resp.getWriter();
+        System.out.println("--delete--");
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
-        JsonObject result = new JsonObject();
+        PrintWriter printWriter = resp.getWriter();
 
         String id = req.getParameter("id");
 
-        if( id != null ){
-            try (Connection connection = DriverManager.getConnection(url, user, password);
-
-                 PreparedStatement preparedStatement = connection.prepareStatement(DELETE_USERS_SQL);) {
-                preparedStatement.setString(1, id);
-
-                int affected = preparedStatement.executeUpdate();
-                System.out.println("Number of records affected :: " + affected);
-
-                if( affected == 0 ) {
-                    result.addProperty( "error", "Invalid Employee Id");
-                } else {
-                    result.addProperty( "message", "Employee with id: " + id + " Successfully Deleted");
-                }
-            } catch (SQLException e) {
-
-                printSQLException(e, resp);
-            }
-        } else {
-            result.addProperty("error", "Please Provide Employee Id");
-        }
-        printWriter.print(result.toString());
-        printWriter.flush();
-    }
-
-    public static void printSQLException(SQLException ex, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter printWriter=resp.getWriter();
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding("UTF-8");
-        JsonObject result = new JsonObject();
-        for (Throwable e: ex) {
-            if (e instanceof SQLException) {
-                e.printStackTrace(System.err);
-                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
-                result.addProperty("SQLState", ((SQLException) e).getSQLState().toString());
-                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
-                result.addProperty("ErrorCode", ((SQLException) e).getErrorCode());
-                System.err.println("Message: " + e.getMessage());
-                result.addProperty("message", e.getMessage().toString());
-                Throwable t = ex.getCause();
-                while (t != null) {
-                    System.out.println("Cause: " + t);
-                    result.addProperty("Cause", t.getMessage().toString());
-                    t = t.getCause();
-                }
-            }
-        }
-        printWriter.print(result.toString());
+        String result = Employee.deleteEmployeeById( id );
+        printWriter.print(result);
         printWriter.flush();
     }
 }

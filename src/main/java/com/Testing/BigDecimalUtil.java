22fd12dfe07237d9b592f47180d770617ad2d4d9
keerthivasan123 <keerthivasan287@gmail.com>
2022-05-26 16:35:58 +0530

    code review changes

--- src/main/java/com/Testing/BigDecimalUtil.java
+++ src/main/java/com/Testing/BigDecimalUtil.java
@@ -6,46 +6,53 @@ import java.util.Iterator;
 import java.util.List;
 
 public class BigDecimalUtil {
-    public static BigDecimal getSumationResult( List<BigDecimal> listOfBigDecimals ){
+    public static final BigDecimal ZERO = BigDecimal.ZERO;
+    public static final BigDecimal ONE = BigDecimal.ONE;
+
+    public static BigDecimal sum( List<BigDecimal> listOfBigDecimals ){
         if( listOfBigDecimals == null ) throw new NullPointerException("Invalid List");
-        BigDecimal result = new BigDecimal("0");
+        BigDecimal result = BigDecimalUtil.ZERO;
         for( BigDecimal num : listOfBigDecimals ){
             result = result.add( num );
         }
         return result;
     }
 
-    public static BigDecimal getSumationResult( BigDecimal num1, BigDecimal num2 ){
+    public static BigDecimal sum( BigDecimal num1, BigDecimal num2 ){
         if( num1 == null || num2 == null ) throw new NullPointerException("Invalid Input");
         return num1.add(num2);
     }
 
-    public static BigDecimal getSubractionResult( List<BigDecimal> listOfBigDecimals ){
+    public static BigDecimal subtract( List<BigDecimal> listOfBigDecimals ){
         if( listOfBigDecimals == null ) throw new NullPointerException("Invalid List");
-        BigDecimal result = new BigDecimal("0");
+        BigDecimal result = BigDecimal.ZERO;
         for( BigDecimal num : listOfBigDecimals ){
             result = result.subtract( num );
         }
         return result;
     }
 
-    public static BigDecimal getSubractionResult( BigDecimal num1, BigDecimal num2 ){
+    public static BigDecimal subtract( BigDecimal num1, BigDecimal num2 ){
         if( num1 == null || num2 == null ) throw new NullPointerException("Invalid Input");
         return num1.subtract(num2);
     }
 
-    public static BigDecimal getMultplierResult( List<BigDecimal> listOfBigDecimals ){
+    public static BigDecimal multiply( List<BigDecimal> listOfBigDecimals ){
         if( listOfBigDecimals == null ) throw new NullPointerException("Invalid List");
-        BigDecimal result = new BigDecimal("1");
+        BigDecimal result = BigDecimal.ONE;
         for( BigDecimal num : listOfBigDecimals ){
             result = result.multiply( num );
         }
         return result;
     }
 
-    public static BigDecimal getMultplierResult( BigDecimal num1, BigDecimal num2 ){
+    public static BigDecimal multiply( BigDecimal num1, BigDecimal num2 ){
         if( num1 == null || num2 == null ) throw new NullPointerException("Invalid Input");
         return num1.multiply(num2);
+    }
+
+    public static BigDecimal convertToNegative( BigDecimal num ){
+        return num.negate();
     }
 
     public static void main(String[] args) {
@@ -54,13 +61,15 @@ public class BigDecimalUtil {
             listObBigDecimals.add(new BigDecimal("11.0"));
             listObBigDecimals.add(new BigDecimal("12.0"));
             listObBigDecimals.add(new BigDecimal("13.0"));
-            System.out.println("Sum of List : " + BigDecimalUtil.getSumationResult( listObBigDecimals ));
-            System.out.println("Difference of List : " + BigDecimalUtil.getSubractionResult( listObBigDecimals ));
-            System.out.println("Multiple of List : " + BigDecimalUtil.getMultplierResult( listObBigDecimals ));
+            System.out.println("Sum of List : " + BigDecimalUtil.sum( listObBigDecimals ));
+            System.out.println("Difference of List : " + BigDecimalUtil.subtract( listObBigDecimals ));
+            System.out.println("Multiple of List : " + BigDecimalUtil.multiply( listObBigDecimals ));
 
-            System.out.println("Sum of Two Number : " + BigDecimalUtil.getSumationResult( new BigDecimal("10"), new BigDecimal("12") ));
-            System.out.println("Difference of Two Number : " + BigDecimalUtil.getSubractionResult( new BigDecimal("15"), new BigDecimal("10") ));
-            System.out.println("Multiple of Two Number : " + BigDecimalUtil.getMultplierResult( new BigDecimal("13"), new BigDecimal("12") ));
+            System.out.println("Sum of Two Number : " + BigDecimalUtil.sum( new BigDecimal("10"), new BigDecimal("12") ));
+            System.out.println("Difference of Two Number : " + BigDecimalUtil.subtract( new BigDecimal("15"), new BigDecimal("10") ));
+            System.out.println("Multiple of Two Number : " + BigDecimalUtil.multiply( new BigDecimal("13"), new BigDecimal("12") ));
+
+            System.out.println("Negative : " + BigDecimalUtil.convertToNegative(new BigDecimal("12")));
         } catch ( Exception e ){
             System.out.println(e.toString());
         }
